dff8a32dfa012f2d2c467d37738653a083a6f151
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-11-04 12:00:22 +0900

    Feat: React Query 기본 세팅

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 
+import ReactQueryProviders from "../config/ReactQueryProvider";
 import GroupPage from "./GroupPage";
 import HomePage from "./HomePage";
 import KeywordPage from "./KeywordPage";
@@ -7,16 +8,18 @@ import MyPage from "./MyPage";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" exact element={<HomePage />} />
-      <Route path="/myPage" element={<MyPage />} />
-      <Route path="/dashboard">
-        <Route path=":groupId">
-          <Route index element={<GroupPage />} />
-          <Route path=":keywordId" element={<KeywordPage />} />
+    <ReactQueryProviders>
+      <Routes>
+        <Route path="/" exact element={<HomePage />} />
+        <Route path="/myPage" element={<MyPage />} />
+        <Route path="/dashboard">
+          <Route path=":groupId">
+            <Route index element={<GroupPage />} />
+            <Route path=":keywordId" element={<KeywordPage />} />
+          </Route>
         </Route>
-      </Route>
-    </Routes>
+      </Routes>
+    </ReactQueryProviders>
   );
 };
 
