d8d75ef47f69f75314eedfc59db5db7026c46af0
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-11-05 23:20:36 +0900

    Style: HomePage 디자인 수정

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,13 +5,16 @@ const HomePage = () => {
   useSignInRedirect();
 
   return (
-    <main className="flex-col-center gap-30 mx-auto mt-80 w-full max-w-1200 px-10 py-100">
-      <Logo styles="text-120 animate-fadeIn hover:cursor-default" type="main" />
-      <section className="flex-col-center gap-10 text-pink-500">
-        <p className="text-20">관심있는 블로그를 구독하여 소식을 받아보세요</p>
-        <p className="text-19">로그인 후 서비스를 이용해주시기 바랍니다</p>
-      </section>
-    </main>
+    <>
+      <main className="flex-col-center gap-15 mx-auto w-full h-screen max-w-1200 px-10">
+        <Logo styles="text-120 animate-fadeIn hover:cursor-default" type="main" />
+        <section className="flex-col-center gap-10 text-pink-500">
+          <p className="text-20">관심있는 블로그를 구독하여 소식을 받아보세요</p>
+          <p className="text-19">로그인 후 서비스를 이용해주시기 바랍니다</p>
+        </section>
+      </main>
+      <footer className="fixed bottom-0 w-full h-80 bg-gradient-to-r from-[#9996EF] to-[#F9C7D4]"></footer>
+    </>
   );
 };
 
