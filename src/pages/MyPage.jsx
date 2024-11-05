eba4b9c43ca5b4c0cc5422d9779c2ec502020fad
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-11-05 11:41:38 +0900

    Feat: 로그인 상태 아닐 시 홈 화면으로 리다이렉트 커스텀 훅 구현

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,4 +1,8 @@
+import useNoSignInRedirect from "../hooks/useNoSignInRedirect";
+
 const MyPage = () => {
+  useNoSignInRedirect();
+
   return <div className="mx-auto mt-80 w-full max-w-1200 px-10">마이페이지</div>;
 };
 
