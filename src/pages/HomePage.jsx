cad04e27d073f7db4974e41d05d1377246b88d27
Ssong-Q <skdmlrh12@gmail.com>
2024-11-07 00:58:41 +0900

    Refactor: 홈페이지 문구 수정 및 Footer 컴포넌트 분리

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Layout/Footer";
 import Logo from "../components/Logo";
 import useSignInRedirect from "../hooks/useSignInRedirect";
 
@@ -9,11 +10,11 @@ const HomePage = () => {
       <main className="flex-col-center gap-15 mx-auto w-full h-screen max-w-1200 px-10">
         <Logo styles="text-120 animate-fadeIn hover:cursor-default" type="main" />
         <section className="flex-col-center gap-10 text-pink-500">
-          <p className="text-20">관심있는 블로그를 구독하여 소식을 받아보세요</p>
+          <p className="text-20">관심있는 키워드를 구독하여 소식을 받아보세요</p>
           <p className="text-19">로그인 후 서비스를 이용해주시기 바랍니다</p>
         </section>
       </main>
-      <footer className="fixed bottom-0 w-full h-80 bg-gradient-to-r from-[#9996EF] to-[#F9C7D4]" />
+      <Footer />
     </>
   );
 };
