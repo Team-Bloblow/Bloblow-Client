f5cd40bdcc11a83da581850aed536df0d68a7ea4
Jintae Song <w.jinttao@gmail.com>
2024-11-07 22:21:31 +0900

    Refactor: CreateKeywordButton 버튼 컴포넌트의 text 동적 제어를 위해 prop 추가

--- src/components/Button/CreateKeywordButton.jsx
+++ src/components/Button/CreateKeywordButton.jsx
@@ -8,7 +8,7 @@ const CreateKeywordButton = ({ isDisabled }) => {
       styles="flex-center px-14 py-8 font-medium border-2 border-purple-200 bg-purple-400/80 rounded-[15px] text-white text-18 hover:bg-purple-500/80"
       isDisabled={isDisabled}
     >
-      생성하기
+      {buttonText}
     </Button>
   );
 };
