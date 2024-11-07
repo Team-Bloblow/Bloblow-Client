ba42064ac32282dc4887e0d1efa2066a427ac072
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-11-07 16:06:17 +0900

    Feat: 유저 그룹 카드 및 리스트 구현

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,3 +1,4 @@
+import UserGroupCardList from "../components/Card/UserGroup/UserGroupCardList";
 import MyPageSidebar from "../components/Sidebar/MyPageSidebar";
 import useNoSignInRedirect from "../hooks/useNoSignInRedirect";
 
@@ -7,7 +8,7 @@ const MyPage = () => {
   return (
     <main className="flex items-start gap-50 mx-auto mt-80 w-full max-w-1200 px-40 py-30">
       <MyPageSidebar />
-      <section className="flex flex-col justify-start gap-10 bg-white border-4 border-pink-200 rounded-[30px] py-40 px-40 w-full h-430 overflow-y-scroll"></section>
+      <UserGroupCardList />
     </main>
   );
 };
