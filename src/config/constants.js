d004de3203d72d19b89f21c1cf99ce7d0eb27628
Jintae Song <w.jinttao@gmail.com>
2024-11-07 22:20:27 +0900

    Feat: 추가 및 제외 키워드 모달 타입, 중복 키워드 안내 문구 상수로 정의

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,6 +2,7 @@ export const BASE_URL = "http://localhost:3000";
 
 export const MODAL_TYPE = Object.freeze({
   CREATE_KEYWORD: "createKeyword",
+  CREATE_SUBKEYWORD: "createSubkeyword",
   CREATE_KEYWORD_SUCCESS: "createKeywordSuccess",
   ERROR: "error",
 });
@@ -10,6 +11,7 @@ export const ERROR_MESSAGE = Object.freeze({
   MUST_GROUP_SELECT: "그룹을 선택해주세요.",
   NEW_GROUP_EMPTY_INPUT_VALUE: "그룹명을 입력해주세요.",
   KEYWORD_EMPTY_INPUT_VALUE: "키워드를 입력해주세요.",
+  KEYWORD_DUPLICATED_INPUT_VALUE: "중복된 키워드가 있어요. 다른 키워드를 입력해주세요.",
   CREATE_KEYWORD_ERROR: "새로운 키워드 생성에 실패하였습니다.",
   SIGN_IN_ERROR: "로그인에 실패하였습니다.",
   FETCH_POSTS: "블로그 정보를 불러오지 못했습니다.",
