6adeb3dbccc96783b1a75934260a28cf483f96f3
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-11-08 17:53:45 +0900

    Feat: 엔드포인트 변경으로 인한 수정

--- src/api/auth/asyncPostSignIn.js
+++ src/api/auth/asyncPostSignIn.js
@@ -3,7 +3,7 @@ import { BASE_URL } from "../../config/constants";
 
 const asyncPostSignIn = async (userInfo) => {
   const fetchInfo = {
-    url: `${BASE_URL}/signIn`,
+    url: `${BASE_URL}/user/signIn`,
     method: "POST",
     params: "",
     body: userInfo,
