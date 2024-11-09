99e55e24a7b749bfc7680e3e0491009c69b0b102
Ssong-Q <skdmlrh12@gmail.com>
2024-11-10 00:47:51 +0900

    Style: 그룹 대시보드 레이아웃 조정

--- src/pages/GroupPage.jsx
+++ src/pages/GroupPage.jsx
@@ -3,7 +3,7 @@ import DashboardSidebar from "../components/Sidebar/DashboardSidebar";
 
 const GroupPage = () => {
   return (
-    <main className="flex justify-start items-start gap-50 mx-auto pt-67 h-screen w-full max-w-1440 pr-40">
+    <main className="flex justify-start items-start mx-auto pt-67 h-screen w-full max-w-1440">
       <DashboardSidebar />
       <section className="w-full h-full flex flex-col justify-start">
         <DashboardHeader />
