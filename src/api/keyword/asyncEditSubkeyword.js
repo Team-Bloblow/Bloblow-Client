5094725f83ab7db8c3513cc287492ec787d66243
Jintae Song <w.jinttao@gmail.com>
2024-11-10 17:18:44 +0900

    Feat: 추가/제외 키워드 편집내용에 따라 DB 업데이트 요청 기능 구현

---
+++ src/api/keyword/asyncEditSubkeyword.js
@@ -0,0 +1,17 @@
+import fetchHandler from "..";
+import { BASE_URL } from "../../config/constants";
+
+const asyncEditSubkeyword = async (subkeywordInfo, keywordId) => {
+  const fetchInfo = {
+    url: `${BASE_URL}/keywords/${keywordId}`,
+    method: "POST",
+    params: "",
+    body: subkeywordInfo,
+  };
+
+  const response = await fetchHandler(fetchInfo);
+
+  return response;
+};
+
+export default asyncEditSubkeyword;
