fc5d83e327c7b71e253f2075deb5d8ff2481369a
HANSOL <hansol302@gmail.com>
2024-11-11 15:14:08 +0900

    Feat: 오늘의 게시물 카드 내 API 연결 추가

--- src/components/Card/Chart/TodayPostCountCard.jsx
+++ src/components/Card/Chart/TodayPostCountCard.jsx
@@ -1,26 +1,39 @@
+import asyncGetTodayPostCount from "../../../api/keyword/asyncGetTodayPostCount";
 import DownArrowIcon from "../../Icon/DownArrowIcon";
 import UpArrowIcon from "../../Icon/UpArrowIcon";
+import { useQuery } from "@tanstack/react-query";
+import PropTypes from "prop-types";
 
-const TodayPostCountCard = () => {
-  const data = {
-    todayPostCount: 18,
-    diffPostCount: 10,
-    diffPercent: 80,
-  };
+const TodayPostCountCard = ({ keywordId }) => {
+  const { data: todayPostData } = useQuery({
+    queryKey: ["todayPostCount", keywordId],
+    queryFn: () => asyncGetTodayPostCount(keywordId),
+  });
+
+  if (typeof todayPostData === "undefined") {
+    return <div className="mt-100 w-1/3 p-10">조회된 게시물이 없습니다 🥲</div>;
+  }
 
   return (
-    <div className="mt-100 w-1/3 p-10">
+    <article className="mt-100 w-1/3 p-10">
       <span>오늘의 게시물</span>
-      <p>어제보다 게시물 수 {Math.abs(data.diffPostCount)}개 {data.diffPostCount >= 0 ? "증가" : "감소"}</p>
+      <p>
+        어제보다 게시물 수 {Math.abs(todayPostData.diffPostCount)}개{" "}
+        {todayPostData.diffPostCount >= 0 ? "증가" : "감소"}
+      </p>
       <div className="flex">
-        <div className="text-50">{data.todayPostCount}</div>
+        <div className="text-50">{todayPostData.todayPostCount}</div>
         <div className="flex">
-          {data.diffPostCount >= 0 ? <UpArrowIcon /> : <DownArrowIcon />}
-          {data.diffPercent}%
+          {todayPostData.diffPostCount >= 0 ? <UpArrowIcon /> : <DownArrowIcon />}
+          {todayPostData.diffPercent}%
         </div>
       </div>
-    </div>
+    </article>
   );
 };
 
 export default TodayPostCountCard;
+
+TodayPostCountCard.propTypes = {
+  keywordId: PropTypes.string,
+};
--- src/pages/KeywordPage.jsx
+++ src/pages/KeywordPage.jsx
@@ -1,5 +1,15 @@
+import { useParams } from "react-router-dom";
+
+import TodayPostCountCard from "../components/Card/Chart/TodayPostCountCard";
+
 const KeywordPage = () => {
-  return;
+  const { keywordId } = useParams();
+
+  return (
+    <div className="flex">
+      <TodayPostCountCard keywordId={keywordId} />
+    </div>
+  );
 };
 
 export default KeywordPage;
