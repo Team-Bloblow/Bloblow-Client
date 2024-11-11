1129926a0a1c7011e6067d2c3ab2cbaf74717535
Jintae Song <w.jinttao@gmail.com>
2024-11-11 11:52:29 +0900

    Feat: KeywordChip 컴포넌트에 선택적으로 close button 적용하는 기능 구현

--- src/components/Chip/KeywordChip.jsx
+++ src/components/Chip/KeywordChip.jsx
@@ -1,12 +1,26 @@
+import CloseIcon from "../Icon/CloseIcon";
+import Button from "../UI/Button";
 import PropTypes from "prop-types";
 
-const KeywordChip = ({ keywordName, styles }) => {
-  return <span className={styles}>{keywordName}</span>;
+const KeywordChip = ({ keywordName, styles, hasCloseButton, handleCloseIconClick }) => {
+  return (
+    <span className={styles}>
+      {keywordName}
+      <div>
+        {hasCloseButton && (
+          <Button styles="right-17" onClick={handleCloseIconClick}>
+            <CloseIcon className="size-25" />
+          </Button>
+        )}
+      </div>
+    </span>
+  );
 };
 
 export default KeywordChip;
 
 KeywordChip.propTypes = {
   keywordName: PropTypes.string.isRequired,
-  styles: PropTypes.string.isRequired,
+  hasCloseButton: PropTypes.bool,
+  handleCloseIconClick: PropTypes.func,
 };
