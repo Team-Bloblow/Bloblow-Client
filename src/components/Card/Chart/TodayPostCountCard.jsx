8c3159c3d3935771f41207241a840cadabad92ad
HANSOL <hansol302@gmail.com>
2024-11-11 16:37:40 +0900

    Refactor: 변수명 변경

--- src/components/Card/Chart/TodayPostCountCard.jsx
+++ src/components/Card/Chart/TodayPostCountCard.jsx
@@ -5,12 +5,12 @@ import { useQuery } from "@tanstack/react-query";
 import PropTypes from "prop-types";
 
 const TodayPostCountCard = ({ keywordId }) => {
-  const { data: todayPostData } = useQuery({
+  const { data: chartData } = useQuery({
     queryKey: ["todayPostCount", keywordId],
     queryFn: () => asyncGetTodayPostCount(keywordId),
   });
 
-  if (typeof todayPostData === "undefined") {
+  if (typeof chartData === "undefined") {
     return <div className="mt-100 w-1/3 p-10">조회된 게시물이 없습니다 🥲</div>;
   }
 
@@ -18,14 +18,14 @@ const TodayPostCountCard = ({ keywordId }) => {
     <article className="mt-100 w-1/3 p-10">
       <span>오늘의 게시물</span>
       <p>
-        어제보다 게시물 수 {Math.abs(todayPostData.diffPostCount)}개{" "}
-        {todayPostData.diffPostCount >= 0 ? "증가" : "감소"}
+        어제보다 게시물 수 {Math.abs(chartData.diffPostCount)}개{" "}
+        {chartData.diffPostCount >= 0 ? "증가" : "감소"}
       </p>
       <div className="flex">
-        <div className="text-50">{todayPostData.todayPostCount}</div>
+        <div className="text-50">{chartData.todayPostCount}</div>
         <div className="flex">
-          {todayPostData.diffPostCount >= 0 ? <UpArrowIcon /> : <DownArrowIcon />}
-          {todayPostData.diffPercent}%
+          {chartData.diffPostCount >= 0 ? <UpArrowIcon /> : <DownArrowIcon />}
+          {chartData.diffPercent}%
         </div>
       </div>
     </article>
