dbd9b19cf4705ea77d5e435df3531136f355feec
HANSOL <hansol302@gmail.com>
2024-11-12 23:58:30 +0900

    Feat: 키워드 대시보드 진입시, 차트 페이지 보이도록 수정

--- src/pages/KeywordPage.jsx
+++ src/pages/KeywordPage.jsx
@@ -1,12 +1,14 @@
-import PostCardList from "../components/Card/Post/PostCardList";
-import DashboardHeader from "../components/Header/DashboardHeader";
-import DashboardSidebar from "../components/Sidebar/DashboardSidebar";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 import PeriodPostCountCard from "../components/Card/Chart/PeriodPostCountCard";
 import TodayPostCountCard from "../components/Card/Chart/TodayPostCountCard";
+import PostCardList from "../components/Card/Post/PostCardList";
+import DashboardHeader from "../components/Header/DashboardHeader";
+import DashboardSidebar from "../components/Sidebar/DashboardSidebar";
 
 const KeywordPage = () => {
+  const [dashboardType] = useState("chart");
   // useNoSignInRedirect();
   const { keywordId } = useParams();
 
@@ -15,9 +17,14 @@ const KeywordPage = () => {
       <DashboardSidebar />
       <section className="w-full h-full flex flex-col justify-start">
         <DashboardHeader />
-        <PostCardList />
-        <TodayPostCountCard keywordId={keywordId} />
-        <PeriodPostCountCard keywordId={keywordId} />
+        {dashboardType === "chart" ? (
+          <div className="flex p-20">
+            <TodayPostCountCard keywordId={keywordId} />
+            <PeriodPostCountCard keywordId={keywordId} />
+          </div>
+        ) : (
+          <PostCardList />
+        )}
       </section>
     </main>
   );
