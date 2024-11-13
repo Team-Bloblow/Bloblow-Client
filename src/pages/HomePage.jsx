7a0cf9a37dd10b38056786af199e56652ab028cb
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-11-13 13:48:01 +0900

    Style: 랜딩 페이지 디자인 수정

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,15 +10,17 @@ const HomePage = () => {
   return (
     <main className="flex flex-col items-center w-full h-screen">
       <div className="w-full h-full flex-grow-1 flex-center gap-30 p-50">
-        <section className="w-full flex flex-col items-start justify-center pl-80 gap-20">
-          <Logo styles="text-100 animate-fadeInFast hover:cursor-default" />
-          <article className="flex flex-col items-start gap-10 animate-fadeInSlow text-gray-800 mb-20">
-            <p className="text-20">관심있는 키워드를 구독하여 소식을 받아보세요</p>
-            <p className="text-20">그룹 내의 여러 키워드 지표를 비교해보세요</p>
-          </article>
-          <SignInButton type={SIGN_BUTTON_TYPE.LANDING_PAGE} />
+        <section className="w-full flex-center pr-100">
+          <div className="flex flex-col items-start justify-center gap-20">
+            <Logo styles="text-100 animate-fadeInFast hover:cursor-default" />
+            <article className="flex flex-col items-start gap-10 animate-fadeInSlow text-gray-800 mb-20">
+              <p className="text-20">관심있는 키워드를 구독하여 소식을 받아보세요</p>
+              <p className="text-20">그룹 내의 여러 키워드 지표를 비교해보세요</p>
+            </article>
+            <SignInButton type={SIGN_BUTTON_TYPE.LANDING_PAGE} />
+          </div>
         </section>
-        <div className="w-500">스크린샷</div>
+        <div className="w-700">스크린샷</div>
       </div>
       <Footer />
     </main>
