5de558020119ee01cbf80c70576d58c91317fe8d
Jintae Song <w.jinttao@gmail.com>
2024-11-15 02:59:11 +0900

    Refactor: 이전 필터링 조건과 동일할 경우 재검색 하지 않도록 수정, 필터링 UI 수정

--- src/components/Card/Post/PostListFilter.jsx
+++ src/components/Card/Post/PostListFilter.jsx
@@ -7,7 +7,7 @@ import Label from "../../UI/Label";
 import PropTypes from "prop-types";
 
 const PostListFilter = ({ filterList, setFilterList }) => {
-  const [subKeywordListAdded, setSubKeywordListAdded] = useState({
+  const [tempFilterList, setTempFilterList] = useState({
     includedKeyword: filterList.includedKeyword,
     excludedKeyword: filterList.excludedKeyword,
   });
@@ -19,116 +19,131 @@ const PostListFilter = ({ filterList, setFilterList }) => {
     includedKeyword: "",
     excludedKeyword: "",
   });
-  const handleSubKeywordInputChange = (e, subKeywordType) => {
-    setInputValue((prev) => ({ ...prev, [subKeywordType]: e.target.value }));
+  const handleFilterInputChange = (e, filterType) => {
+    setInputValue((prev) => ({ ...prev, [filterType]: e.target.value }));
     return;
   };
-  const handleSubKeywordEnterSubmit = (e, subKeywordType) => {
+  const handleCreateTempFilterSubmit = (e, filterType) => {
     e.preventDefault();
-    if (inputValue[subKeywordType]?.trim() === "") {
+    if (inputValue[filterType]?.trim() === "") {
       return;
     }
-    const hasSubKeyword = Object.values(subKeywordListAdded).some((subKeyword) =>
-      subKeyword.includes(inputValue[subKeywordType])
+    const hasFilter = Object.values(tempFilterList).some((filter) =>
+      filter.includes(inputValue[filterType])
     );
-    if (hasSubKeyword) {
+    if (hasFilter) {
       setErrorMessage((prev) => ({
         ...prev,
-        [subKeywordType]: ERROR_MESSAGE.KEYWORD_DUPLICATED_INPUT_VALUE,
+        [filterType]: ERROR_MESSAGE.KEYWORD_DUPLICATED_INPUT_VALUE,
       }));
       return;
     }
 
-    setSubKeywordListAdded((prev) => ({
+    setTempFilterList((prev) => ({
       ...prev,
-      [subKeywordType]: [...prev[subKeywordType], inputValue[subKeywordType]],
+      [filterType]: [...prev[filterType], inputValue[filterType]],
     }));
-    setInputValue((prev) => ({ ...prev, [subKeywordType]: "" }));
-    setErrorMessage((prev) => ({ ...prev, [subKeywordType]: "" }));
+    setInputValue((prev) => ({ ...prev, [filterType]: "" }));
+    setErrorMessage((prev) => ({ ...prev, [filterType]: "" }));
     return;
   };
-  const handleSubKeywordChipRemoveButtonClick = (subKeywordType, subKeywordNameForRemove) => {
-    setSubKeywordListAdded((prev) => ({
+  const handleFilterChipRemoveButtonClick = (filterType, filterForRemove) => {
+    setTempFilterList((prev) => ({
       ...prev,
-      [subKeywordType]: prev[subKeywordType].filter(
-        (subKeywordName) => subKeywordName !== subKeywordNameForRemove
-      ),
+      [filterType]: prev[filterType].filter((filter) => filter !== filterForRemove),
     }));
     return;
   };
   const handleFilterApplyButtonClick = () => {
-    const isEqualQueryResult =
-      Object.values(filterList).join() === Object.values(subKeywordListAdded).join();
-    if (isEqualQueryResult) {
-      alert("현재 검색 결과와 동일해요");
+    const filterListSet = new Set(Object.values(filterList).flat());
+    const tempFilterListSet = new Set(Object.values(tempFilterList).flat());
+    const hasFilter = Array.from(tempFilterListSet).some((filter) => filterListSet.has(filter));
+
+    if (hasFilter) {
       return;
     }
-    setFilterList(subKeywordListAdded);
+    setFilterList(tempFilterList);
     return;
   };
   return (
-    <div className="relative w-full h-210">
-      <div className="absolute top-0 right-20 w-8/12 h-40 my-15 px-15 border-2 font-semibold">
-        <form onSubmit={(e) => handleSubKeywordEnterSubmit(e, "includedKeyword")}>
-          <Label
-            htmlFor="includedKeyword"
-            styles="w-100 text-15 text-slate-700 font-semibold flex-shrink-0 hover:text-emerald-900/80"
+    <div className="flex flex-col gap-10 w-full px-20 py-10">
+      <div className="flex flex-col gap-5">
+        <div className="flex items-center gap-15 w-full">
+          <form
+            className="w-400 h-40 px-20 flex items-center gap-5 flex-shrink-0 border-2 rounded-md"
+            onSubmit={(e) => handleCreateTempFilterSubmit(e, "includedKeyword")}
           >
-            포함
-          </Label>
-          {subKeywordListAdded?.includedKeyword?.map((subKeyword) => {
-            return (
-              <KeywordChip
-                key={subKeyword}
-                keywordName={subKeyword}
-                hasCloseButton={true}
-                onClick={() => handleSubKeywordChipRemoveButtonClick("includedKeyword", subKeyword)}
-              />
-            );
-          })}
-          <input
-            type="text"
-            id="includedKeyword"
-            value={inputValue.includedKeyword}
-            onChange={(e) => handleSubKeywordInputChange(e, "includedKeyword")}
-            className="w-6/12 h-35 px-15 font-semibold outline-none"
-            placeholder="어떤 키워드를 포함할까요?"
-          />
-        </form>
-        <p className="text-15 text-red-400 h-18 font-semibold">{errorMessage.includedKeyword}</p>
+            <Label
+              htmlFor="includedKeyword"
+              styles="text-15 text-slate-700 font-semibold flex-shrink-0 hover:text-emerald-900/80"
+            >
+              포함
+            </Label>
+            <input
+              type="text"
+              id="includedKeyword"
+              value={inputValue.includedKeyword}
+              onChange={(e) => handleFilterInputChange(e, "includedKeyword")}
+              className="w-full flex-grow-1 h-full px-10 font-semibold outline-none"
+              placeholder="포함할 키워드"
+            />
+          </form>
+          <div className="flex items-center w-full h-full">
+            {tempFilterList?.includedKeyword?.map((subKeyword) => {
+              return (
+                <KeywordChip
+                  key={subKeyword}
+                  keywordName={subKeyword}
+                  hasCloseButton={true}
+                  onClick={() => handleFilterChipRemoveButtonClick("includedKeyword", subKeyword)}
+                  styles="px-10 py-5 m-5 border-solid border-2 rounded-xl bg-green-100 border-green-200"
+                />
+              );
+            })}
+          </div>
+        </div>
+        <p className="text-14 text-red-400 font-semibold">{errorMessage.includedKeyword}</p>
       </div>
-      <div className="absolute bottom-30 right-20 w-8/12 h-40 my-15 px-15 border-2 font-semibold">
-        <form onSubmit={(e) => handleSubKeywordEnterSubmit(e, "includedKeyword")}>
-          <Label
-            htmlFor="excludedKeyword"
-            styles="w-100 text-15 text-slate-700 font-semibold flex-shrink-0 hover:text-emerald-900/80"
+      <div className="flex flex-col gap-5">
+        <div className="flex items-center gap-15 w-full">
+          <form
+            className="w-400 h-40 px-20 flex items-center gap-5 flex-shrink-0 border-2 rounded-md"
+            onSubmit={(e) => handleCreateTempFilterSubmit(e, "excludedKeyword")}
           >
-            제외
-          </Label>
-          {subKeywordListAdded?.excludedKeyword?.map((subKeyword) => {
-            return (
-              <KeywordChip
-                key={subKeyword}
-                keywordName={subKeyword}
-                hasCloseButton={true}
-                onClick={() => handleSubKeywordChipRemoveButtonClick("excludedKeyword", subKeyword)}
-              />
-            );
-          })}
-          <input
-            type="text"
-            id="excludedKeyword"
-            value={inputValue.excludedKeyword}
-            onChange={(e) => handleSubKeywordInputChange(e, "excludedKeyword")}
-            className="w-6/12 h-35 px-15 font-semibold outline-none"
-            placeholder="어떤 키워드를 제외할까요?"
-          />
-        </form>
-        <p className="text-15 text-red-400 h-18 font-semibold">{errorMessage.excludedKeyword}</p>
+            <Label
+              htmlFor="excludedKeyword"
+              styles="text-15 text-slate-700 font-semibold flex-shrink-0 hover:text-emerald-900/80"
+            >
+              제외
+            </Label>
+            <input
+              type="text"
+              id="excludedKeyword"
+              value={inputValue.excludedKeyword}
+              onChange={(e) => handleFilterInputChange(e, "excludedKeyword")}
+              className="w-full flex-grow-1 h-full px-10 font-semibold outline-none"
+              placeholder="제외할 키워드"
+            />
+          </form>
+          <div className="flex items-center w-full h-full">
+            {tempFilterList?.excludedKeyword?.map((subKeyword) => {
+              return (
+                <KeywordChip
+                  key={subKeyword}
+                  keywordName={subKeyword}
+                  hasCloseButton={true}
+                  onClick={() => handleFilterChipRemoveButtonClick("excludedKeyword", subKeyword)}
+                  styles="px-10 py-3 m-5 border-solid border-2 rounded-xl bg-red-100 border-red-200"
+                />
+              );
+            })}
+          </div>
+        </div>
+        <p className="text-14 text-red-400 font-semibold">{errorMessage.excludedKeyword}</p>
       </div>
       <Button
         onClick={handleFilterApplyButtonClick}
-        styles="absolute bottom-0 right-20 px-50 py-5 rounded-[5px] font-medium text-gray-900/80 border-t-2 border-b-2 border-slate-200/80 font-semibold hover:bg-emerald-100/10 hover:border-emerald-900/20"
+        styles="w-200 right-20 px-50 py-5 rounded-[5px] font-medium text-gray-900/80 border-2 border-slate-200/80 font-semibold hover:bg-emerald-100/10 hover:border-emerald-900/20"
       >
         필터 적용
       </Button>
