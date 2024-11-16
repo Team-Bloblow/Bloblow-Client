1c16df1007dc8547b785a973ad6f491e392ac4c1
Jintae Song <w.jinttao@gmail.com>
2024-11-17 02:32:44 +0900

    Feat: 정렬, 광고 필터 fetchHandler 파라미터 추가

--- src/api/post/asyncGetPosts.js
+++ src/api/post/asyncGetPosts.js
@@ -5,8 +5,10 @@ const asyncGetPosts = async (
   cursorId = POST_LISTS.DEFAULT_CURSOR_ID,
   {
     keywordId,
+    order = POST_LISTS.DEFAULT_ORDER,
     includedKeyword = POST_LISTS.DEFAULT_INCLUDED_KEYWORD,
     excludedKeyword = POST_LISTS.DEFAULT_EXCLUDED_KEYWORD,
+    isAd = POST_LISTS.DEFAULT_IS_AD,
     limit = POST_LISTS.DEFAULT_LIMIT,
   }
 ) => {
@@ -14,7 +16,7 @@ const asyncGetPosts = async (
   const excludedKeywordParams = excludedKeyword.length === 0 ? "" : excludedKeyword.join();
   const fetchInfo = {
     url: `${BASE_URL}/posts/${keywordId}`,
-    params: `?includedKeyword=${includedKeywordParams}&excludedKeyword=${excludedKeywordParams}&limit=${limit}&cursorId=${cursorId}`,
+    params: `?order=${order}&includedKeyword=${includedKeywordParams}&excludedKeyword=${excludedKeywordParams}&isAd=${isAd}$limit=${limit}&cursorId=${cursorId}`,
   };
 
   const response = await fetchHandler(fetchInfo);
