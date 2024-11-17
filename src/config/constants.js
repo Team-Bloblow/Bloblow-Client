8c18562be4a324a844ce6bdbad55b299bee0ff3a
Jintae Song <w.jinttao@gmail.com>
2024-11-18 01:27:42 +0900

    Feat: 이전과 동일한 게시물 필터 적용시 안내 메시지 노출

--- src/config/constants.js
+++ src/config/constants.js
@@ -17,6 +17,7 @@ export const ERROR_MESSAGE = Object.freeze({
   KEYWORD_EMPTY_INPUT_VALUE: "키워드를 입력해주세요.",
   CREATE_KEYWORD_ERROR: "새로운 키워드 생성에 실패하였습니다.",
   KEYWORD_DUPLICATED_INPUT_VALUE: "이미 키워드가 필터로 등록되어 있어요.",
+  APPLIED_POST_FILTER: "이미 필터가 적용되었어요.",
   SIGN_IN_ERROR: "로그인에 실패하였습니다.",
   FETCH_POSTS: "블로그 정보를 불러오지 못했습니다.",
 });
