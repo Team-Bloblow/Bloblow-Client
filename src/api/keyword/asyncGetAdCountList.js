885d82bddc8f614386fdcb95610ee14aed430ad3
HANSOL <hansol302@gmail.com>
2024-11-17 19:14:06 +0900

    Feat: 광고 여부 차트 내 기간 설정 버튼 추가 및 기능 구현

--- src/api/keyword/asyncGetAdCountList.js
+++ src/api/keyword/asyncGetAdCountList.js
@@ -1,10 +1,10 @@
 import fetchHandler from "..";
 import { BASE_URL } from "../../config/constants";
 
-const asyncGetAdCountList = async (keywordId, cursorId) => {
+const asyncGetAdCountList = async (keywordId, cursorId, period) => {
   const fetchInfo = {
     url: `${BASE_URL}/posts/keywords/${keywordId}/adCount`,
-    params: `?cursorId=${cursorId}`,
+    params: `?cursorId=${cursorId}&period=${period}`,
   };
 
   const response = await fetchHandler(fetchInfo);
--- src/components/Card/Chart/PeriodAdCountCard.jsx
+++ src/components/Card/Chart/PeriodAdCountCard.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
 import asyncGetAdCountList from "../../../api/keyword/asyncGetAdCountList";
+import { PERIOD_TYPE } from "../../../config/constants";
+import PeriodToggleButton from "../../Button/PeriodToggleButton";
 import StackBarChart from "../../Chart/StackBarChart";
 import PeriodPagination from "../../Pagination/PeriodPagination";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
@@ -8,14 +10,15 @@ import PropTypes from "prop-types";
 
 const PeriodAdCountCard = ({ keywordId }) => {
   const [cursorId, setCursorId] = useState("");
+  const [period, setPeriod] = useState(PERIOD_TYPE.WEEKLY);
 
   const {
     data: chartData,
     isPlaceholderData,
     isError,
   } = useQuery({
-    queryKey: ["adCount", keywordId, cursorId],
-    queryFn: () => asyncGetAdCountList(keywordId, cursorId),
+    queryKey: ["adCount", keywordId, period, cursorId],
+    queryFn: () => asyncGetAdCountList(keywordId, cursorId, period),
     placeholderData: keepPreviousData,
   });
 
@@ -35,6 +38,7 @@ const PeriodAdCountCard = ({ keywordId }) => {
     <article className="flex flex-col gap-6 w-1/2 h-full p-10 border-2 rounded-md">
       <span className="flex-shrink-0 bg-green-100/20 px-10 py-5 rounded-[2px]">
         광고 여부
+        <PeriodToggleButton period={period} setPeriod={setPeriod} setCursorId={setCursorId} />
       </span>
       <div className="flex-col-center">
         <StackBarChart chartData={chartData} />
