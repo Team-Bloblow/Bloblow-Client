9fee14c9e7528d8bc502adc19833a5bd3e943ad0
Jintae Song <w.jinttao@gmail.com>
2024-11-19 04:36:49 +0900

    Design: 랜딩페이지 수정 및 Header 추가

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -5,16 +5,14 @@ import Logo from "../Common/Logo";
 
 const Header = () => {
   const location = useLocation();
-  const pathname = location.pathname;
-
-  if (pathname === "/") {
-    return null;
-  }
+  const pathName = location.pathname;
 
   return (
     <header className="flex-center fixed top-0 z-header w-full border-b-1 bg-white shadow-sm">
-      <div className="mt-2 flex h-64 w-full max-w-1150 items-center justify-between px-40">
-        <Logo styles="text-36" destination="/" />
+      <div
+        className={`flex items-center justify-between mt-2 h-64 w-full ${pathName === "/" ? "px-50" : "max-w-1150 px-40"}`}
+      >
+        <Logo styles="p-5 m-10 w-50 h-50" destination="/" />
         <SignInButton />
       </div>
     </header>
