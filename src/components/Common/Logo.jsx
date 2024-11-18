4c457973cd1caa46fe0eef06abe53a71bc4da32b
Jintae Song <w.jinttao@gmail.com>
2024-11-19 04:36:08 +0900

    Logo 컴포넌트 수정 및 이미지 교체

--- src/components/Common/Logo.jsx
+++ src/components/Common/Logo.jsx
@@ -1,14 +1,10 @@
-import { SIGNATURE_COLOR } from "../../config/constants";
 import Button from "../UI/Button";
 import PropTypes from "prop-types";
 
 const Logo = ({ styles, destination }) => {
   return (
-    <Button
-      destination={destination}
-      styles={`font-bold bg-gradient-to-r from-[${SIGNATURE_COLOR.START}] via-[${SIGNATURE_COLOR.VIA}] via-40% to-[${SIGNATURE_COLOR.TO}] text-transparent bg-clip-text ${styles}`}
-    >
-      Bloblow
+    <Button destination={destination} styles={styles}>
+      <img src="../public/assets/logo-bloblow.png" alt="logo-bloblow"></img>
     </Button>
   );
 };
