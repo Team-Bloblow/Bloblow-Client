28b20f2ab9e1e40d8a46f39b4e160fd554d62944
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-11-19 18:28:13 +0900

    Style: 대시보드 사이드바와 차트 섹션 높이 일치

--- src/pages/GroupPage.jsx
+++ src/pages/GroupPage.jsx
@@ -44,19 +44,11 @@ const GroupPage = () => {
   }
 
   return (
-    <main className="flex justify-start items-start mx-auto pt-67 h-screen w-full max-w-1440">
-      <DashboardSidebar
-        userGroupList={userGroupList?.groupListResult}
-        userUid={userUid}
-        groupId={groupId}
-      />
-      <section className="flex flex-col justify-start w-full">
-        <DashboardHeader
-          userGroupList={userGroupList?.groupListResult}
-          userUid={userUid}
-          groupId={groupId}
-        />
-        <article className="flex flex-col border-l-1 border-b-2 border-r-2 border-slate-200/80 shadow-md w-full mb-30">
+    <main className="flex justify-start items-stretch mx-auto pt-67 w-full h-full max-w-1440">
+      <DashboardSidebar userGroupList={userGroupList?.groupListResult} groupId={groupId} userUid={userUid} />
+      <section className="flex flex-col justify-stretch w-full">
+        <DashboardHeader userGroupList={userGroupList?.groupListResult} groupId={groupId} userUid={userUid} />
+        <article className="flex flex-col border-l-1 border-b-2 border-r-2 border-slate-200/80 shadow-md w-full">
           <div className="flex flex-col gap-10 p-10 w-full">
             <GroupPeriodPostCountCard
               groupChartType={GROUP_CHART_TYPE.POST}
