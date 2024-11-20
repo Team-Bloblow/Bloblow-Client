837c9d7fd4b6cf1b8576ced5e0e602e86b5ed0f3
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-11-20 14:20:24 +0900

    Refactor: 랜딩페이지 문구 수정

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -17,9 +17,7 @@ const HomePage = () => {
               <p className="text-26 font-bold">관심있는 키워드의 블로그 게시물만 쏙</p>
               <div className="flex flex-col items-start gap-10">
                 <p className="text-20">키워드를 구독하여 게시물 소식을 받아보세요</p>
-                <p className="text-20">
-                  매일매일 업데이트 되는 여러 지표의 추이를 분석할 수 있어요
-                </p>
+                <p className="text-20">원하는대로 게시물들을 필터링할 수 있어요</p>
               </div>
             </article>
             <SignInButton type={SIGN_BUTTON_TYPE.LANDING_PAGE} />
@@ -31,10 +29,10 @@ const HomePage = () => {
         <div className="flex-col-center w-[40%]">스크린샷</div>
         <section className="w-[60%] flex-center pr-100">
           <div className="flex flex-col items-start justify-center gap-20 animate-fadeInSlow text-gray-800">
-            <p className="text-26 font-bold">그룹 내 키워드들의 지표를 비교해 보아요</p>
+            <p className="text-26 font-bold">매일매일 업데이트 되는 여러 지표에 대한 추이</p>
             <div className="flex flex-col items-start gap-10">
-              <p className="text-20">키워드들을 카테고리화하여 하나의 그룹으로 만들 수 있어요</p>
-              <p className="text-20"></p>
+              <p className="text-20">대시보드를 활용하여 수치화된 지표들을 분석할 수 있어요</p>
+              <p className="text-20">그룹화한 키워드 간의 지표를 비교해보세요</p>
             </div>
           </div>
         </section>
