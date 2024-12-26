c74c178f5357a14f3cb48f5c05e48207009b01a7
Jintae Song <w.jinttao@gmail.com>
2024-12-26 21:42:09 +0900

    Refactor: 그룹 목록이 없을 때 케이스 보완

--- src/components/Sidebar/GroupSummary.jsx
+++ src/components/Sidebar/GroupSummary.jsx
@@ -3,20 +3,27 @@ import getDateDiff from "../../utils/getDateDiff";
 
 const GroupSummary = () => {
   const userGroupList = useBoundStore((state) => state.userGroupList);
-  const groupLatestUpdated = userGroupList.reduce((prev, curr) => {
-    return new Date(prev.updatedAt) <= new Date(curr.updatedAt) ? curr : prev;
-  });
+  const groupLatestUpdated =
+    userGroupList.length === 0
+      ? {}
+      : userGroupList?.reduce((prev, curr) => {
+          return new Date(prev.updatedAt) <= new Date(curr.updatedAt) ? curr : prev;
+        });
   const passedDate = getDateDiff("today", groupLatestUpdated.updatedAt);
 
   return (
     <div className="flex flex-col gap-20 w-full h-full px-15 lg:px-15 py-10 lg:py-15">
       <div className="flex justify-between items-end">
         <span className="text-20 font-semibold">최근 업데이트</span>
-        <span className="text-12 font-light text-gray-500">{passedDate}일 전</span>
+        <span className="text-12 font-light text-gray-500">
+          {passedDate ? `${passedDate}일 전` : null}
+        </span>
       </div>
       <div className="flex justify-between">
         <span className="text-17 font-light text-gray-600">그룹</span>
-        <span className="text-17 font-semibold">{groupLatestUpdated.name}</span>
+        <span className="text-17 font-semibold">
+          {groupLatestUpdated.name ? groupLatestUpdated.name : "-"}
+        </span>
       </div>
       <div className="flex justify-between">
         <span className="text-15 font-light text-gray-600">키워드명</span>
