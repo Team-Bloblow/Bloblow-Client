199143ee3192228eb387d14df0451528c945e45b
Kyukyoung Song <skdmlrh12@gmail.com>
2024-12-27 11:48:54 +0900

    Feat: 크롤링 수동 버튼 관련 비동기 api 함수 구현

---
+++ src/api/crawl/asyncPostKeywords.js
@@ -0,0 +1,17 @@
+import fetchHandler from "..";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+const asyncPostKeywords = async (keywordId) => {
+  const fetchInfo = {
+    url: `${API_BASE_URL}/keywords`,
+    method: "POST",
+    params: keywordId,
+  };
+
+  const response = await fetchHandler(fetchInfo);
+
+  return response;
+};
+
+export default asyncPostKeywords;
